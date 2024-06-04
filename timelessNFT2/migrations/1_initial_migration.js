263d618f603fa1683a948bc68c0c335e66389e96
kiqsmg <kiqsmg@gmail.com>
2024-06-04 10:46:54 -0300

    Create new timelessNFT folder

---
+++ timelessNFT2/migrations/1_initial_migration.js
@@ -0,0 +1,17 @@
+// Deploying a smart contract to the Ethereum blockchain using Truffle ( a development framework for Ethereum).
+
+const Migrations = artifacts.require("Migrations"); // is a Truffle function that retrieves the compiled contract artifact for the "Migrations" contract
+
+
+
+//module export statement for the deployment script. It makes the function available to Truffle when running deployment commands
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+};
+
+
+/*
+this code is a migration script that tells Truffle to deploy the Migrations contract to the blockchain.
+ This is typically one of the initial steps in setting up a new project with Truffle,
+ ensuring that the deployment process can be managed effectively.
+*/
