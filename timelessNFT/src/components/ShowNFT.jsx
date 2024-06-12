e224cba114c65dcc3c6e817ad88de8cf13af599a
kiqsmg <kiqsmg@gmail.com>
2024-06-12 17:17:53 -0300

    Commit the ShowNFT component code flow

--- timelessNFT/src/components/ShowNFT.jsx
+++ timelessNFT/src/components/ShowNFT.jsx
@@ -1,113 +1,55 @@
-// Overall this component allows users to fill in details for an NFT, upload an image, and mint the NFT by interacting with IPFS and blockchain services
+// Overall this component renders a modal that displays NFT details and allows the user to either purchase the NFT or change its price, depending on whether they are the owner
 
-//Import functions from store
-import {
-  useGlobalState,
-  setGlobalState,
-  setLoadingMsg,
-  setAlert,
-} from '../store'
-//import useState from React to manage component state              WHAT IS COMPONENT STATE?????????
-import { useState } from 'react'
+
+import Identicon from 'react-identicons'
 import { FaTimes } from 'react-icons/fa'
-//Import create from the http-client, which is used to interract with the IPFS                 what??????????
-import { create } from 'ipfs-http-client'
-// Not sure what this actually do ???????
-import { mintNFT } from '../Blockchain.Services'
+import { useGlobalState, setGlobalState, truncate, setAlert } from '../store'
+// Function that handles the purchase of an NFT on the blockchain (probably)
+import { buyNFT } from '../Blockchain.Services'
 
+const ShowNFT = () => {
+  // Use custom hooks to retrieve global state ariables such as seen bellow...
+  const [showModal] = useGlobalState('showModal') //control modal visibility
+  const [connectedAccount] = useGlobalState('connectedAccount') //the current connected blockchain account
+  const [nft] = useGlobalState('nft') //the NFT details to display
 
-// Constructs a basic authorization header using environment variables        what???????????
-const auth =
-  'Basic ' +
-  Buffer.from(
-    process.env.REACT_APP_INFURIA_PID + ':' + process.env.REACT_APP_INFURIA_API,
-  ).toString('base64')
+  //Hide showmodal and shows the updated price model
+  const onChangePrice = () => {
+    setGlobalState('showModal', 'scale-0')
+    setGlobalState('updateModal', 'scale-100')
+  }
 
-
-  //Creates an IPFS client configured to connect to the Infura IPFS service using the authorization header defined earlier      what?????
-const client = create({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https',
-  headers: {
-    authorization: auth,
-  },
-})
-
-const CreateNFT = () => {
-  //Use custom hooks to define and manage state variables        how do state variables work again ???????
-  const [modal] = useGlobalState('modal')
-  const [title, setTitle] = useState('')
-  const [price, setPrice] = useState('')
-  const [description, setDescription] = useState('')
-  const [fileUrl, setFileUrl] = useState('')
-  const [imgBase64, setImgBase64] = useState(null)
-
-  //Define an asynchronous function that handles the form submission, check empty forms, catches any errors, and UPLOAD FILE TO IPFS AND GET METADATA URI AND MINT THE NFT  ??????????????
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if (!title || !price || !description) return
-
-    setGlobalState('modal', 'scale-0')
-    setGlobalState('loading', { show: true, msg: 'Uploading IPFS data...' })
+  //Defines an asychronous function that handles the NFT purchase process. Hide showmodal and shows loading state with a message
+  const handleNFTPurchase = async () => {
+    setGlobalState('showModal', 'scale-0')
+    setGlobalState('loading', {
+      show: true,
+      msg: 'Initializing NFT transfer...',
+    })
 
     try {
-      const created = await client.add(fileUrl)
-      const metadataURI = `https://ipfs.io/ipfs/${created.path}`
-      const nft = { title, price, description, metadataURI }
-
-      setLoadingMsg('Intializing transaction...')
-      setFileUrl(metadataURI)
-      await mintNFT(nft)
-
-      resetForm()
-      setAlert('Minting completed...', 'green')
+      await buyNFT(nft)
+      setAlert('Transfer completed...', 'green')
       window.location.reload()
     } catch (error) {
-      console.log('Error uploading file: ', error)
-      setAlert('Minting failed...', 'red')
+      console.log('Error transfering NFT: ', error)
+      setAlert('Purchase failed...', 'red')
     }
-  }
-
-  // Asynchronous function that handles image file changes
-  const changeImage = async (e) => {
-    const reader = new FileReader()
-    if (e.target.files[0]) reader.readAsDataURL(e.target.files[0])
-
-    reader.onload = (readerEvent) => {
-      const file = readerEvent.target.result
-      setImgBase64(file)
-      setFileUrl(e.target.files[0])
-    }
-  }
-
-  const closeModal = () => {
-    setGlobalState('modal', 'scale-0')
-    resetForm()
-  }
-
-  const resetForm = () => {
-    setFileUrl('')
-    setImgBase64(null)
-    setTitle('')
-    setPrice('')
-    setDescription('')
   }
 
   return (
     <div
       className={`fixed top-0 left-0 w-screen h-screen flex items-center
-        justify-center bg-black bg-opacity-50 transform
-        transition-transform duration-300 ${modal}`}
+          justify-center bg-black bg-opacity-50 transform
+          transition-transform duration-300 ${showModal}`}
     >
       <div className="bg-[#151c25] shadow-xl shadow-[#e32970] rounded-xl w-11/12 md:w-2/5 h-7/12 p-6">
-        <form className="flex flex-col">
+        <div className="flex flex-col">
           <div className="flex flex-row justify-between items-center">
-            <p className="font-semibold text-gray-400">Add NFT</p>
+            <p className="font-semibold text-gray-400">Buy NFT</p>
             <button
               type="button"
-              onClick={closeModal}
+              onClick={() => setGlobalState('showModal', 'scale-0')}
               className="border-0 bg-transparent focus:outline-none"
             >
               <FaTimes className="text-gray-400" />
@@ -115,98 +57,73 @@ const CreateNFT = () => {
           </div>
 
           <div className="flex flex-row justify-center items-center rounded-xl mt-5">
-            <div className="shrink-0 rounded-xl overflow-hidden h-20 w-20">
+            <div className="shrink-0 rounded-xl overflow-hidden h-40 w-40">
               <img
-                alt="NFT"
                 className="h-full w-full object-cover cursor-pointer"
-                src={
-                  imgBase64 ||
-                  'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'
-                }
+                src={nft?.metadataURI}
+                alt={nft?.title}
               />
             </div>
           </div>
-  
-          <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
-            <label className="block">
-              <span className="sr-only">Choose profile photo</span>
-              <input
-                type="file"
-                accept="image/png, image/gif, image/jpeg, image/webp"
-                className="block w-full text-sm text-slate-500
-                  file:mr-4 file:py-2 file:px-4
-                  file:rounded-full file:border-0
-                  file:text-sm file:font-semibold
-                  file:bg-[#19212c] file:text-gray-400
-                  hover:file:bg-[#1d2631]
-                  cursor-pointer focus:ring-0 focus:outline-none"
-                onChange={changeImage}
-                required
-              />
-            </label>
-          </div>
 
-          <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
-            <input
-              className="block w-full text-sm
-                text-slate-500 bg-transparent border-0
-                focus:outline-none focus:ring-0"
-              type="text"
-              name="title"
-              placeholder="Title"
-              onChange={(e) => setTitle(e.target.value)}
-              value={title}
-              required
-            />
-          </div>
+          <div className="flex flex-col justify-start rounded-xl mt-5">
+            <h4 className="text-white font-semibold">{nft?.title}</h4>
+            <p className="text-gray-400 text-xs my-1">{nft?.description}</p>
 
-          <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
-            <input
-              className="block w-full text-sm
-                text-slate-500 bg-transparent border-0
-                focus:outline-none focus:ring-0"
-              type="number"
-              step={0.01}
-              min={0.01}
-              name="price"
-              placeholder="Price (Eth)"
-              onChange={(e) => setPrice(e.target.value)}
-              value={price}
-              required
-            />
-          </div>
+            <div className="flex justify-between items-center mt-3 text-white">
+              <div className="flex justify-start items-center">
+                <Identicon
+                  string={nft?.owner}
+                  size={50}
+                  className="h-10 w-10 object-contain rounded-full mr-3"
+                />
+                <div className="flex flex-col justify-center items-start">
+                  <small className="text-white font-bold">@owner</small>
+                  <small className="text-pink-800 font-semibold">
+                    {nft?.owner ? truncate(nft.owner, 4, 4, 11) : '...'}
+                  </small>
+                </div>
+              </div>
 
-          <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
-            <textarea
-              className="block w-full text-sm resize-none
-                text-slate-500 bg-transparent border-0
-                focus:outline-none focus:ring-0 h-20"
-              type="text"
-              name="description"
-              placeholder="Description"
-              onChange={(e) => setDescription(e.target.value)}
-              value={description}
-              required
-            ></textarea>
+              <div className="flex flex-col">
+                <small className="text-xs">Current Price</small>
+                <p className="text-sm font-semibold">{nft?.cost} ETH</p>
+              </div>
+            </div>
           </div>
-
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="flex flex-row justify-center items-center
-              w-full text-white text-md bg-[#e32970]
-              hover:bg-[#bd255f] py-2 px-5 rounded-full
-              drop-shadow-xl border border-transparent
-              hover:bg-transparent hover:text-[#e32970]
-              hover:border hover:border-[#bd255f]
-              focus:outline-none focus:ring mt-5"
-          >
-            Mint Now
-          </button>
-        </form>
+          <div className="flex justify-between items-center space-x-2">
+            {connectedAccount == nft?.owner ? (
+              <button
+                className="flex flex-row justify-center items-center
+                w-full text-[#e32970] text-md border-[#e32970]
+                py-2 px-5 rounded-full bg-transparent 
+                drop-shadow-xl border hover:bg-[#bd255f]
+                hover:bg-transparent hover:text-white
+                hover:border hover:border-[#bd255f]
+                focus:outline-none focus:ring mt-5"
+                onClick={onChangePrice}
+              >
+                Change Price
+              </button>
+            ) : (
+              <button
+                className="flex flex-row justify-center items-center
+                w-full text-white text-md bg-[#e32970]
+                hover:bg-[#bd255f] py-2 px-5 rounded-full
+                drop-shadow-xl border border-transparent
+                hover:bg-transparent hover:text-[#e32970]
+                hover:border hover:border-[#bd255f]
+                focus:outline-none focus:ring mt-5"
+                onClick={handleNFTPurchase}
+              >
+                Purchase Now
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default CreateNFT
+export default ShowNFT
