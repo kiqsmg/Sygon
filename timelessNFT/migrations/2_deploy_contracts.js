550a2ba7a41160d731899ace569b62401b5b894c
kiqsmg <kiqsmg@gmail.com>
2024-06-15 11:46:07 -0300

    Add comments on the migrations deploy code

--- timelessNFT/migrations/2_deploy_contracts.js
+++ timelessNFT/migrations/2_deploy_contracts.js
@@ -1,3 +1,8 @@
+// This script is a deployment script for a Truffle project. It imports the 'TimelessNFT' smart contract,
+// retrieves the list of available Ethereum accounts, and deploys the 'TimelessNFT' contract wth specific parameters (name, symbol, account address).
+// It is suposed to run during the deployment process
+
+
 /* eslint-disable no-undef */
 const TimelessNFT = artifacts.require('TimelessNFT')
 
