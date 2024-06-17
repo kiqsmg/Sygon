379ba011934fe494163598d8efdbf2a3705dfb02
kiqsmg <kiqsmg@gmail.com>
2024-06-17 16:11:36 -0300

    Set back the ShownNFT component to old/incomplete version

--- timelessNFT/src/components/ShowNFT.jsx
+++ timelessNFT/src/components/ShowNFT.jsx
@@ -5,13 +5,13 @@ import Identicon from 'react-identicons'
 import { FaTimes } from 'react-icons/fa'
 import { useGlobalState, setGlobalState, truncate, setAlert } from '../store'
 // Function that handles the purchase of an NFT on the blockchain (probably)
-import { buyNFT } from '../Blockchain.Services'
+// import { buyNFT } from '../Blockchain.Services'
 
 const ShowNFT = () => {
   // Use custom hooks to retrieve global state ariables such as seen bellow...
   const [showModal] = useGlobalState('showModal') //control modal visibility
-  const [connectedAccount] = useGlobalState('connectedAccount') //the current connected blockchain account
-  const [nft] = useGlobalState('nft') //the NFT details to display
+  // const [connectedAccount] = useGlobalState('connectedAccount') //the current connected blockchain account
+  // const [nft] = useGlobalState('nft') //the NFT details to display
 
   //Hide showmodal and shows the updated price model
   const onChangePrice = () => {
@@ -20,8 +20,9 @@ const ShowNFT = () => {
   }
 
   //Defines an asychronous function that handles the NFT purchase process. Hide showmodal and shows loading state with a message
-  const handleNFTPurchase = async () => {
+  const handleNFTPurchase = () => {
     setGlobalState('showModal', 'scale-0')
+    /*
     setGlobalState('loading', {
       show: true,
       msg: 'Initializing NFT transfer...',
@@ -35,6 +36,7 @@ const ShowNFT = () => {
       console.log('Error transfering NFT: ', error)
       setAlert('Purchase failed...', 'red')
     }
+    */
   }
 
   return (
@@ -60,65 +62,66 @@ const ShowNFT = () => {
             <div className="shrink-0 rounded-xl overflow-hidden h-40 w-40">
               <img
                 className="h-full w-full object-cover cursor-pointer"
-                src={nft?.metadataURI}
-                alt={nft?.title}
+                src="https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDYvNGE4NmNmOWQtODM2Mi00YmVhLThiMzctZDEyODAxNjUxZTE1LmpwZWc=.jpg"
+                alt="Title"
               />
             </div>
           </div>
 
           <div className="flex flex-col justify-start rounded-xl mt-5">
-            <h4 className="text-white font-semibold">{nft?.title}</h4>
-            <p className="text-gray-400 text-xs my-1">{nft?.description}</p>
+            <h4 className="text-white font-semibold">Title</h4>
+            <p className="text-gray-400 text-xs my-1">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              tempora laborum quod.
+            </p>
 
             <div className="flex justify-between items-center mt-3 text-white">
               <div className="flex justify-start items-center">
                 <Identicon
-                  string={nft?.owner}
+                  string={'ugjkhjlk'}
                   size={50}
                   className="h-10 w-10 object-contain rounded-full mr-3"
                 />
                 <div className="flex flex-col justify-center items-start">
                   <small className="text-white font-bold">@owner</small>
                   <small className="text-pink-800 font-semibold">
-                    {nft?.owner ? truncate(nft.owner, 4, 4, 11) : '...'}
+                    0xe2...a1f6
                   </small>
                 </div>
               </div>
 
               <div className="flex flex-col">
                 <small className="text-xs">Current Price</small>
-                <p className="text-sm font-semibold">{nft?.cost} ETH</p>
+                <p className="text-sm font-semibold">0.34 ETH</p>
               </div>
             </div>
           </div>
-          <div className="flex justify-between items-center space-x-2">
-            {connectedAccount == nft?.owner ? (
-              <button
-                className="flex flex-row justify-center items-center
-                w-full text-[#e32970] text-md border-[#e32970]
-                py-2 px-5 rounded-full bg-transparent 
-                drop-shadow-xl border hover:bg-[#bd255f]
-                hover:bg-transparent hover:text-white
-                hover:border hover:border-[#bd255f]
-                focus:outline-none focus:ring mt-5"
-                onClick={onChangePrice}
-              >
-                Change Price
-              </button>
-            ) : (
-              <button
-                className="flex flex-row justify-center items-center
-                w-full text-white text-md bg-[#e32970]
-                hover:bg-[#bd255f] py-2 px-5 rounded-full
-                drop-shadow-xl border border-transparent
-                hover:bg-transparent hover:text-[#e32970]
-                hover:border hover:border-[#bd255f]
-                focus:outline-none focus:ring mt-5"
-                onClick={handleNFTPurchase}
-              >
-                Purchase Now
-              </button>
-            )}
+
+          <div className='flex justify-between items-center space-x-2'>
+            <button
+              className="flex flex-row justify-center items-center
+               w-full text-white text-md bg-[#e32970]
+               hover:bg-[#bd255f] py-2 px-5 rounded-full
+               drop-shadow-xl border border-transparent
+               hover:bg-transparent hover:text-[#e32970]
+               hover:border hover:border-[#bd255f]
+               focus:outline-none focus:ring mt-5"
+               onClick={handleNFTPurchase}
+            >
+              Purchase Now
+            </button>
+            <button
+              className="flex flex-row justify-center items-center
+              w-full text-[#e32970] text-md border-[#e32970]
+              py-2 px-5 rounded-full bg-transparent
+              drop-shadow-xl border hover:bg-[#bd255f]
+              hover:bg-transparent hover:text-white
+              hover:border hover:border-[#bd255f]
+              focus:outline-none focus:ring mt-5"
+              onClick={onChangePrice}
+            >
+              Change Price
+            </button>
           </div>
         </div>
       </div>
