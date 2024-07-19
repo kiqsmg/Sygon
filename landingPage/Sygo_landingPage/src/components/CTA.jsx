1d4c1d2bf8d63db6ff5a2e67bb8803b2955074a1
kiqsmg <kiqsmg@gmail.com>
2024-07-18 23:56:45 -0300

    Fix some text errors in CTA

--- landingPage/Sygo_landingPage/src/components/CTA.jsx
+++ landingPage/Sygo_landingPage/src/components/CTA.jsx
@@ -6,8 +6,8 @@ import Button from "./Button";
 const CTA = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Compre ou venda créditos de carbono no nosso marketplace!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Tudo que você precisa para investir em crédito de carbono de maneira simples e sem preocupações.</p>
+      <h2 className={styles.heading2}>Compre e venda créditos de carbono no nosso marketplace!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Tudo que você precisa para investir em créditos de carbono de maneira simples e sem preocupações.</p>
     </div>
 
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-8`}>
