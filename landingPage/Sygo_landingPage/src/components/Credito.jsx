1c4d464926e74cb1fd3251d93d69ca5a5392b779
kiqsmg <kiqsmg@gmail.com>
2024-08-07 16:51:40 -0300

    Fix some minor text errors in Mercado and Credito sections

--- landingPage/Sygo_landingPage/src/components/Credito.jsx
+++ landingPage/Sygo_landingPage/src/components/Credito.jsx
@@ -5,6 +5,16 @@ import { polution } from "../assets";
 const Credito = () => (
   <section id="carbono" className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row  flex-col rounded-[20px] box-shadow`}>
 
+    <div className="flex-1 flex flex-col mb-10">
+      <h2 className={styles.heading2}>Crédito de carbono no Brasil</h2>
+
+      <ul className="list-inside list-disc mt-5 ml-5 font-poppins font-normal text-dimWhite text-[18px] leading-[24px]">
+        <li className="mb-5">Credito de crédito no Brasil: R$ 150 MILHÕES/ANO</li>
+        <li className="mb-5">Brasil tem 15% do potencial global de captura de carbono</li>
+        <li className="mb-5">Potencial para produzir US$125 BI em créditos</li>
+      </ul>
+    </div>
+
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 mr-20 relative`}>
         <img src={polution} alt="billing" className="w-[80%] h-[80%] relative z-[5] rounded-lg" />
 
@@ -13,17 +23,6 @@ const Credito = () => (
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
-    </div>
-
-
-    <div className="flex-1 flex flex-col mb-10">
-      <h2 className={styles.heading2}>Credito de carbono no Brasil</h2>
-
-      <ul className="list-inside list-disc mt-5 font-poppins font-normal text-dimWhite text-[18px] leading-[24px]">
-        <li className="mb-5">Credito de crédito no Brasil: R$ 150 MILHÕES/ANO</li>
-        <li className="mb-5">Brasil tem 15% do potencial global de captura de carbono</li>
-        <li className="mb-5">Potencial para produzir US$125 BI em créditos</li>
-      </ul>
     </div>
 
   </section>
