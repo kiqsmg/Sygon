b01d42dd0d28d9ad322dfeb98297800db435c9dc
kiqsmg <kiqsmg@gmail.com>
2024-10-24 12:12:50 -0300

    Add new player image and the projects images

--- landingPage/Sygo_landingPage/src/assets/index.js
+++ landingPage/Sygo_landingPage/src/assets/index.js
@@ -13,6 +13,9 @@ import instagram from "./instagram.svg";
 import logo_grande from "./logo_grande.svg";
 import teamplayer01 from "./teamplayer01.jpeg";
 import teamplayer02 from "./teamplayer02.jpeg";
+import teamplayer03 from "./teamplayer03.jpeg";
+import project01 from "./project01.jpg";
+import project02 from "./project02.avif";
 
 import whatsapp from "./whatsapp.svg";
 import tiktok from "./tiktok.svg";
@@ -37,6 +40,9 @@ export {
   tiktok,
   teamplayer01,
   teamplayer02,
+  teamplayer03,
+  project01,
+  project02,
   verde,
   credito,
 };
