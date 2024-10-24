a368a9556804c3d0bd1cac19b56d447bb364f4b8
kiqsmg <kiqsmg@gmail.com>
2024-10-24 11:53:09 -0300

    Add Projects section

--- landingPage/Sygo_landingPage/src/components/Projects.jsx
+++ landingPage/Sygo_landingPage/src/components/Projects.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { projects } from "../constants";
 import styles from "../style";
-import Button from "./Button";
+import ProjectCard from './ProjectCard';
+
 
 
 const Projects = () => (
-  <section id="Projects" className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient rounded-[20px] box-shadow`}>
-    <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Compre e venda créditos de carbono no nosso marketplace!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Tudo que você precisa para investir em créditos de carbono de maneira simples e sem preocupações.</p>
+  
+  <section id="equipe" className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 mt-20`}>
+
+    <div className="flex flex-row justify-between items-center text-center w-full">
+      <h1 className="flex-1 font-poppins font-semibold ss:text-[50px] text-[32px] text-white ss:leading-[100px] leading-[40px] mb-10">
+        <span className="text-gradient">Conheça nosso time</span>
+      </h1>
     </div>
 
-    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-8`}>
-      <Button />
+    <p className="font-poppins font-semibold text-dimWhite mb-10 text-center max-w-[800px] ">Nossa equipe foi construida com a ideia principal de "tornar preservação um negócio lucrativo",
+       assim nossos fundadores se juntaram para solucionar dores do mercado de carbono. Contamos com membros especializados,
+       em financeiro, jurídico e desenvolvimento.</p>
+
+    <div className="flex flex-wrap justify-center w-full feedback-container relative z-[1] mt-10 ml-12 gap-20 sm:gap-0 ">
+      {teamplayers.map((card) => <TeamCard key={card.id} {...card} />)}
     </div>
   </section>
 )
-
-export default CTA
+export default Projects
