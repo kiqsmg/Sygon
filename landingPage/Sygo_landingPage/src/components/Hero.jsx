60b945e3017fb82bdc9a1ebe93aff22f49db3b4f
kiqsmg <kiqsmg@gmail.com>
2024-11-18 22:41:26 -0300

    Make changes in the font, sizes and colors

--- landingPage/Sygo_landingPage/src/components/Hero.jsx
+++ landingPage/Sygo_landingPage/src/components/Hero.jsx
@@ -9,17 +9,16 @@ const Hero = () => (
       
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-10`}>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[50px] text-[30px] text-black ss:leading-[100px] leading-[40px] ">
-            Tornando a <span className="text-gradient">sustentabilidade,</span> <br className="sm:block hidden"/>
-            em um mercado lucrativo!
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[110px] text-[40px] text-black ss:leading-[100px] leading-[40px] ">
+            Tornando a <br className="sm:block hidden"/>sustentabilidade <br className="sm:block hidden"/>lucrativa!
           </h1>
         </div>
 
-        <p className={`${styles.paragraph} max-w-[620px] mt-5 mb-5 ss:text-[24px]`}>
-          Criamos soluções de sustentabilidade que tornam a redução de <span className="text-gradient text-[26px]">impactos ambientais</span> lucrativa para empresas e <span className="text-gradient text-[26px]">acessível</span> para os consumidores.
+        <p className={`${styles.paragraph} max-w-[760px] mt-10 mb-5 ss:text-[36px]`}>
+        Nós criamos soluções que tornam as práticas de ESG <span className="font-bold text-[38px]">lucrativas</span> para as empresas e <span className="font-bold text-[38px]">acessíveis</span> para os consumidores.
         </p>
 
-        <Button styles="mt-10 ml-12"></Button>
+        <Button styles="mt-10 bg-forth"></Button>
       </div>
 
     </div>
