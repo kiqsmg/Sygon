f80bdd40c0827314444bf1bbf51a7cee4dabeb98
kiqsmg <kiqsmg@gmail.com>
2024-11-19 11:18:09 -0300

    Restructure the stats section to fit the new layout

--- landingPage/Sygo_landingPage/src/components/Stats.jsx
+++ landingPage/Sygo_landingPage/src/components/Stats.jsx
@@ -3,14 +3,11 @@ import { stats } from "../constants";
 import styles from "../style";
 
 
-
 const Stats = () => (
-  
-  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+  <section className={`flex flex-row flex-wrap sm:mb-20 sm:mt-10 ml-10 mb-6`}>
     {stats.map((stat) => (
-      <div key={stat.id} className={`flex justify-start items-center flex-row m-12`}>
-        <h4 className=" font-poppins font-semibold xs:text-[32px] text-[22px] xs:leading-[43px] leading-[33px] text-black">{stat.value}</h4>
-        {/* <p className= " font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">{stat.title}</p> */}
+      <div key={stat.id} className={`flex justify-start items-center flex-row m-10`}>
+        <h4 className= {`font-bold xs:text-[32px] text-[22px] xs:leading-[43px] leading-[33px] text-third cursor-pointer transition duration-200 hover:scale-110`}>{stat.value}</h4>
       </div>
     ))}
   </section>
