1389ba2e6703727a76496aafec07e584e11d966d
kiqsmg <kiqsmg@gmail.com>
2024-11-20 23:59:11 -0300

    Refactor stats section

--- landingPage/Sygo_landingPage/src/components/Stats.jsx
+++ landingPage/Sygo_landingPage/src/components/Stats.jsx
@@ -4,7 +4,7 @@ import styles from "../style";
 
 
 const Stats = () => (
-  <section className={`flex flex-row flex-wrap sm:mb-20 sm:mt-10 ml-10 mb-6`}>
+  <section className={`flex flex-row flex-wrap sm:mb-10 sm:mt-10 ml-10 mb-6`}>
     {stats.map((stat) => (
       <div key={stat.id} className={`flex justify-start items-center flex-row m-10`}>
         <h4 className= {`font-bold xs:text-[32px] text-[22px] xs:leading-[43px] leading-[33px] text-third cursor-pointer transition duration-200 hover:scale-110`}>{stat.value}</h4>
